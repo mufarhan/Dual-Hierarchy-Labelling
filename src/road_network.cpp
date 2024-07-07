6d887341588c2f1f778dfe511a149d079111f574
Muhammad Farhan <mufarhan.pervaiz@gmail.com>
2024-07-07 15:46:29 +1000

    Update road_network.cpp

--- src/road_network.cpp
+++ src/road_network.cpp
@@ -2889,7 +2889,7 @@ void Graph::contract_seq(ContractionIndex &ci, vector<pair<pair<distance_t,dista
     }
 }
 
-#ifdef MULTI_THREAD
+#ifdef MULTI_THREAD_DISTANCES
 void Graph::DhclDec_Par(ContractionHierarchy &ch, ContractionIndex &ci, vector<pair<pair<distance_t, distance_t>, pair<NodeID, NodeID> > >& updates) {
 
     vector<thread> threads;
